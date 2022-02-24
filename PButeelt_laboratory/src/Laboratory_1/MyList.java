dd236023e784557a814302878e36bf6191b14c0f
ok <ok@192.168.1.15>
2022-02-24 20:23:44 +0800

    3 functions written by Unuuu

--- PButeelt_laboratory/src/Laboratory_1/MyList.java
+++ PButeelt_laboratory/src/Laboratory_1/MyList.java
@@ -4,11 +4,44 @@ public class MyList {
 	protected Object[] mass;
 	protected int size;
 	
+	//element = ChangeArrayLength.changeLength1D(element, 2 * size);
 	public MyList(int cap) {
 		if(cap > 0) {
 			mass = new Object[cap];
 		}
 		else System.out.println("capacity must be higher than zero");
 	}
+	void checkIndex(int index)
+	   {
+	      if (index < 0 || index >= size)
+	         throw new IndexOutOfBoundsException
+	               ("index = " + index + "  size = " + size);
+	   }
+	public Object remove(int index)
+	   {
+	      checkIndex(index);
+	      Object removed = mass[index];
+	      for (int i = index + 1; i < size; i++)	mass[i-1] = mass[i];
+	      mass[--size] = null;
+	      return removed;
+	   }
+	public boolean isEmpty() {
+		return size ==0; 
+	}
+	public String toString(){
+		String s = "{";
+		checkIndex(size);
+		for(int i=0;i<size;i++) {
+			if(mass[i]==null) s=s+"null";
+			else s=s+mass[i];
+			s=s+" ,";
+		}
+		s=s.substring(0,s.length() -2);
+		s=s+'}';
+		return s;
+	}
+	
+	
+	
 	
 }
