2a01db22834d3635d1ccf57a79609700b8956c1a
Start Small <Start Small@192.168.1.75>
2022-02-27 12:55:18 +0800

    boltson ooy

--- PButeelt_laboratory/src/Laboratory_1/MyList.java
+++ PButeelt_laboratory/src/Laboratory_1/MyList.java
@@ -48,14 +48,17 @@ public class MyList {
 		checkIndex(index);
 		mass[index]=elemento;
 	}
-public void push(int new_data)
-{
-	Object checkIndex = new Object();
-	Object head;
-	Object new_node;
-	new_node.next = head;
-	head = new_node;
-}
-}
+	public void insertAfter(Node prev_node, int new_data)
+	{
+		if (prev_node == null)
+	    {
+	        System.out.println("The given previous node cannot be null");
+	        return;
+	    }
+		Node new_node = new Node(new_data);
+		new_node.next = prev_node.next;
+		prev_node.next = new_node;
+	}
+	}
 
 
